4d04406eded246fca4e505d6b89d002398b4871c
Pramodjena <pramoodjena@gmail.com>
2024-10-07 16:01:06 +0530

    class-2 notes and live-codes

--- Frontend/Javascript/class-2/data.js
+++ Frontend/Javascript/class-2/data.js
@@ -1,8 +1,83 @@
 // Null
 // let user;
 
-let user = null;
+// let user = null;
 // var user = null;
 // const user = null;
-console.log(typeof null); // object
-console.log(user);
+// console.log(typeof null); // object
+// console.log(user);
+
+// Undefined
+// let data;
+// console.log(typeof data); // undefined
+// console.log(data);
+
+// Number
+// let num1 = 50;
+// let decimal_no = 1.0001;
+// let pi = 3.14;
+// let age = 25;
+// let roll_no = 21;
+
+// console.log(typeof num1); // number
+
+// Addition
+// let num2 = 100;
+// let num3 = 125;
+// let addition = num2 + num3;
+// console.log(addition);
+
+// todo: addition, subtraction, multiplication and division then push it into your gitHub account. Once you done share me the link in the group.
+
+// BigInt
+// let huge_no = 111111111111111111111115555555555555555555555558445644545454545454545454545454545454;
+// console.log(typeof huge_no);
+
+// Symbol
+// let sym1 = Symbol("sym1");
+// let sym2 = Symbol("sym1");
+// console.log(sym1 === sym2); // false
+
+// NaN(Not a Number)
+// let num5 = 0;
+// let num6 = 0;
+// console.log(num5 / num6);
+
+// let num7 = "Pramod";
+// let num8 = 10;
+// console.log(num7 / num8); // NaN
+
+// let num9 = 0;
+// let num10 = 10;
+// console.log(num10 / num9);
+
+// Boolean
+
+// let isMarried = false;
+// let isSingle = true;
+// console.log(typeof isMarried); // boolean
+// console.log(typeof isSingle); // boolean
+// console.log(isMarried);
+// console.log(isSingle);
+
+// String
+// let name = "Pramod";
+// let age = 25;
+// let city = "Bhubaneswar";
+// console.log(name);
+// console.log(age);
+// console.log(city);
+// console.log(`My name is: ${name}, age is: ${age}, and city is:${city}`);
+
+// To check the length of the string
+// console.log(city.length); // 11
+
+// let my_website = "www.qlith.com";
+// console.log(my_website.replace(".", "-"));
+
+// Type Coercion:
+
+// let score1 = "100";
+// let score2 = 200;
+// let coercedAddition = score1 + score2;
+// console.log(coercedAddition); // "100200" (due to coercion)
---
+++ Frontend/Javascript/class-2/question.md
@@ -0,0 +1,28 @@
+# Questions :
+
+## Question-1:
+
+Write a JavaScript program that calculates the total bill for a restaurant order. The program should do the following:
+
+- Define two variables `foodPrice` and `drinkPrice` and assign them any values of your choice.
+- Calculate the total bill by adding `foodPrice` and `drinkPrice`.
+- Add a 10% tip based on the total bill.
+- Display the final amount (total bill + tip) using `console.log`.
+
+## Question-2:
+
+Write a JavaScript program that calculates the final price of a product after applying a discount. The program should:
+
+- Define a variable `originalPrice` to store the original price of the product.
+- Define another variable `discount` to store the discount amount.
+- Calculate the final price by subtracting the `discount` from the original price.
+- Display the `discounted` price using `console.log`.
+
+## Question-3:
+
+Write a JavaScript program that calculates the total amount a customer needs to pay after adding sales tax to the product price. The program should:
+
+- Define a variable `productPrice` for the cost of the product.
+- Define a variable `salesTaxRate` to store the sales tax percentage (e.g., 8%).
+- Calculate the tax amount and the total price (product price + tax).
+- Display the total price after adding sales tax using `console.log`.
