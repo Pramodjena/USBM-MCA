49cf9f83f827262bd4d4e4bbcc9011a6dffd514f
Pramodjena <pramoodjena@gmail.com>
2024-10-28 23:58:53 +0530

    array upadted

---
+++ Frontend/Javascript/class-7/array.js
@@ -0,0 +1,59 @@
+// Array :
+
+// Array Declartion :
+
+// let numbers = [1, 2, 3, 4, 5];
+// console.log(numbers);
+
+// let newNumbers = new Array(1, 2.5, 5.66, 100);
+// console.log(newNumbers);
+
+// Array Methods :
+
+// length :
+// console.log(numbers.length); // 5
+
+// Accessing :
+let cities = ["Bhubaneswar", "Cuttack", "Balesore", "Jajpur", "Bhadrak"];
+// console.log(cities[0]); // Bhubaneswar
+// console.log(cities[4]); // Bhadrak
+
+// Last element :
+
+// let lastCity = cities.length - 1; // 4
+// console.log(cities[lastCity]);
+
+// Balesore
+// console.log(cities[2]);
+// console.log(cities[lastCity / 2]);
+// console.log(cities[lastCity - 2]);
+
+// Modify :
+
+// console.log(cities);
+// let modifyCity = "Kendrapada";
+// cities[2] = modifyCity;
+// console.log(cities);
+
+// Add element :
+// push() :
+// console.log(cities);
+// let addCity = "Jagatsinghpur";
+// cities.push(addCity);
+// console.log(cities);
+
+// Remove :
+// pop() :
+// console.log(cities);
+// cities.pop();
+// console.log(cities);
+
+// shift()
+console.log(cities);
+cities.shift();
+console.log(cities);
+
+// unshift()
+console.log(cities);
+cities.unshift("Bripada");
+console.log(cities);
