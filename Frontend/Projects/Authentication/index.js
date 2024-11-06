27c020106567dd002a828d3f921a21c48efe2515
Pramodjena <pramoodjena@gmail.com>
2024-11-06 11:24:55 +0530

    auth upadated

--- Frontend/Projects/Authentication/index.js
+++ Frontend/Projects/Authentication/index.js
@@ -3,14 +3,24 @@ const users = JSON.parse(localStorage.getItem("users")) || [];
 // To manipulate HTML elements
 const signupBtn = document.getElementById("signupBtn");
 const loginBtn = document.getElementById("loginBtn");
+const logoutBtn = document.getElementById("logoutBtn");
 
 // Display dashboard if the user is logged in
+function showView(view) {
+  document.getElementById("signup").style.display =
+    view === "signup" ? "block" : "none";
+  document.getElementById("login").style.display =
+    view === "login" ? "block" : "none";
+  document.getElementById("dashboard").style.display =
+    view === "dashboard" ? "block" : "none";
+}
 
 // Show view
 
 // Add events
 signupBtn.addEventListener("click", signUp);
 loginBtn.addEventListener("click", login);
+logoutBtn.addEventListener("click", logout);
 
 function signUp() {
   const username = document.getElementById("username").value;
@@ -33,11 +43,47 @@ function signUp() {
   users.push(newUser);
   localStorage.setItem("users", JSON.stringify(users));
   alert("Signup successful! You can now log in.");
-  console.log(newUser);
+  showView("login");
+
+  // document.getElementById("username").value = "";
+  // document.getElementById("useremail").value = "";
+  // document.getElementById("userepassword").value = "";
 }
 
 // login
+function login() {
+  const useremail = document.getElementById("useremail").value;
+  const userpassword = document.getElementById("userepassword").value;
+
+  const storedUsers = JSON.parse(localStorage.getItem("users")) || [];
+  const user = storedUsers.find(
+    (user) => user.email === useremail && user.password === userpassword
+  );
+
+  if (user) {
+    const loggedUser = localStorage.setItem("loggedUser", JSON.stringify(user));
+    alert("Login successful!");
+    showDashboard();
+  } else {
+    alert("Invalid credentials");
+  }
+
+  // document.getElementById("useremail").value = "";
+  // document.getElementById("userepassword").value = "";
+}
 
 // Show dashboard with a welcome message
+function showDashboard() {
+  const user = JSON.parse(localStorage.getItem("loggedUser"));
+  document.getElementById(
+    "usernameDisplay"
+  ).innerText = `Hey ${user.name} welcome !!`;
+  showView("dashboard");
+}
 
 // Logout function
+function logout() {
+  localStorage.removeItem("loggedUser");
+  alert("You've successfully logged out !!");
+  showView("signup");
+}
