974eb5304514452aa3a701b62a14d720a551826e
Pramodjena <pramoodjena@gmail.com>
2024-12-10 13:34:50 +0530

    node modules

--- Backend/class-1/index.js
+++ Backend/class-1/index.js
@@ -1,5 +1,32 @@
 // Node.js
-
 console.log("It's working");
-console.warn("It's working");
-console.error("It's working");
+
+// Import modules
+const math = require("./math");
+const fs = require("fs");
+
+// custom modules
+console.log(math.add(5, 6));
+console.log(math.sub(11, 6));
+
+// in-built modules
+console.log(fs);
+
+//write file
+fs.writeFileSync("notes.txt", "I am learning backend");
+
+// append : add the string end of the file
+fs.appendFileSync("notes.txt", "It's part of MERN \n");
+
+// read :
+const data = fs.readFileSync("notes.txt");
+console.log(data.toString());
+
+// create folder :
+fs.mkdirSync("file.js");
+
+// rename :
+fs.renameSync("file.js", "node.js");
+
+// delete
+fs.rmdirSync("node.js");
